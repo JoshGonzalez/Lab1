8114d880bae18db389a25535f6ed451501350a71
JoshGonzalez <56143006+JoshGonzalez@users.noreply.github.com>
2020-09-03 21:56:58 -0700

    Update Computation.swift

--- Restaurant/Computation.swift
+++ Restaurant/Computation.swift
@@ -1,3 +1,11 @@
 import Foundation
 
-// Place your code here
+func tip(subTotal: Double, tipPercentage: Double) ->Double{
+  return subTotal * tipPercentage
+}
+func taxes(subTotal: Double) ->Double{
+  return subTotal * 0.075
+}
+func total(subTotal: Double, tipPercentage: Double) ->Double{
+  return subTotal + tip(subTotal: subTotal, tipPercentage: tipPercentage) + taxes(subTotal: subTotal)
+}
